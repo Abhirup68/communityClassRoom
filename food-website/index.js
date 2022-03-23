3f3763d371635da041644dfb100ee40e90c2b689
Abhirup68 <102205666+Abhirup68@users.noreply.github.com>
2022-03-23 10:57:25 +0530

    Add files via upload

---
+++ food-website/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+     slideToShow:3,
+     slidesToScroll:1,
+     prevArrow:".prev-btn",
+     nextArrow:".next-btn"
+    });
+});
